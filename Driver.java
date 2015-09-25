29fc2f087b2fd4f4fe8efa2b9ec17e4893fcb807
Billy <bwong5@stuy.edu>
2015-09-25 11:44:41 -0400

    Modified the Driver.java file

--- Driver.java
+++ Driver.java
@@ -1,12 +1,21 @@
-public class Driver {
-    public static void main {
-	//Java files can compile classes from different Java files from same directory as an OBJECT
-	Greeter g1;
-	g1 = new Greeter();
-	g2 = new Greeter();
-	g1.message = "HELLLO!";
-	g2.message = "Hola!";
-	g1.greet();
-	g2.greet();
-    } 
+//we have a separate file Driver.java (or any other name) 
+//This class has a main method and will create Greeter objects
+public class Driver{
+    public static void main(String[]args){
+        //DECLARE
+        Greeter g1,g2;
+        //INSTANTIATE (initialize an object)
+        g1 = new Greeter();
+        g2 = new Greeter();
+
+        //set the message
+        g1.message = "Hey sup!";
+        g2.message = "Hola!";
+
+        //make g1 and g2 use their greet method
+        g1.greet("Bob");
+        g2.greet("Dylan");
+        g1.greet("Annie");
+
+    }
 }
